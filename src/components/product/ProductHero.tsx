0e5ee9bf0063723101dd48c214d270632da332a1
tanner <tanner.spanu08@gmail.com>
2025-03-08 10:07:45 -0600

    fixed product button

--- src/components/product/ProductHero.tsx
+++ src/components/product/ProductHero.tsx
@@ -75,7 +75,7 @@ export function ProductHero({ product, hasPurchased }: ProductHeroProps) {
         {!hasPurchased ? (
           <PurchaseButton 
             productId={product.id}
-            price={product.price}
+            price={product.price ?? 0}
           />
         ) : (
           <div className="flex gap-2.5">
