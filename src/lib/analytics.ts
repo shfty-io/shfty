dfc91e4f0e45d1cf59f290c6758d3cf3e948c8e8
tanner <tanner.spanu08@gmail.com>
2025-03-24 21:25:47 -0500

    added google analytics

---
+++ src/lib/analytics.ts
@@ -0,0 +1,30 @@
+// Google Analytics implementation
+type GTagEvent = {
+  action: string;
+  category: string;
+  label: string;
+  value?: number;
+};
+
+// Initialize Google Analytics
+export const GA_MEASUREMENT_ID = process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID;
+
+// https://developers.google.com/analytics/devguides/collection/gtagjs/pages
+export const pageview = (url: string) => {
+  if (typeof window !== 'undefined' && GA_MEASUREMENT_ID) {
+    window.gtag('config', GA_MEASUREMENT_ID, {
+      page_path: url,
+    });
+  }
+};
+
+// https://developers.google.com/analytics/devguides/collection/gtagjs/events
+export const event = ({ action, category, label, value }: GTagEvent) => {
+  if (typeof window !== 'undefined' && GA_MEASUREMENT_ID) {
+    window.gtag('event', action, {
+      event_category: category,
+      event_label: label,
+      value: value,
+    });
+  }
+}; 
